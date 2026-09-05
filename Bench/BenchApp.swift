eaff415c16e1b3fa4e4f927f69d822735c6a3c9f
Greentea <112603484+iluvicecream@users.noreply.github.com>
2026-09-05 18:52:22 +0700

    Initial Commit

---
+++ Bench/BenchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenchApp.swift
+//  Bench
+//
+//  Created by perr on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct BenchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bench/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bench
+//
+//  Created by perr on 9/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
